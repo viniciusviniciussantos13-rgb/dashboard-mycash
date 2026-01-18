098044c6dfe4a9d7d3c9b45d55a1d0c6ac248df7
Vinicius Botelho <viniciusbotelho@gmail.com>
2026-01-18 15:36:55 -0300

    fix: Remove gap entre sidebar e navbar - navbar colada ao sidebar (pl-0 no desktop)

--- src/components/dashboard/DashboardHeader/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader/DashboardHeader.tsx
@@ -29,7 +29,8 @@ export default function DashboardHeader() {
         flex flex-col lg:flex-row lg:items-center lg:justify-between
         gap-2 lg:gap-0
         w-full mb-6
-        px-6 pt-6
+        pt-6
+        pl-0 lg:pl-0 pr-6 lg:pr-6
       "
       data-name="Navbar"
     >
